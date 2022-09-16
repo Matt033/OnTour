687dc366b33ab1aa4f8287f2b94b30af34d80536
Kwanwoo Baek <kwanwoob@usc.edu>
2022-09-16 11:58:41 -0700

    Added Scheduling

--- ontour/src/pages/Artist.jsx
+++ ontour/src/pages/Artist.jsx
@@ -16,6 +16,64 @@ function Artist() {
 
           <div class="container py-5">
             <h2 class="h3 font-weight-bold">Upcoming Shows</h2>
+            <div class="d-flex justify-content-center">
+            <ul class="list-group list-width">
+              <li class="list-group-item">
+                <div class="row">
+                  <div class="col-1">
+                  <button type="button" class="date-btn btn btn-secondary">
+                    <h6>
+                      Sep 16
+                    </h6>
+                  </button>
+                  </div>
+                  <div class="col-3">
+                    <div class="px-5 font-weight-bold">
+                      City, State
+                    </div>
+                  </div>
+                  <div class="col-8">
+                    hi
+                  </div>
+                </div>
+              </li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Morbi leo risus</li>
+              <li class="list-group-item">Porta ac consectetur ac</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            </div> 
+          </div>
+
+          <div class="container py-5">
+            <h2 class="h3 font-weight-bold">Past Shows</h2>
+            <div class="d-flex justify-content-center">
+            <ul class="list-group list-width">
+              <li class="list-group-item">
+                <div class="row">
+                  <div class="col-1">
+                  <button type="button" class="date-btn btn btn-secondary">
+                    <h6>
+                      Sep 16
+                    </h6>
+                  </button>
+                  </div>
+                  <div class="col-3">
+                    <div class="px-5 font-weight-bold">
+                      City, State
+                    </div>
+                  </div>
+                  <div class="col-8">
+                    hi
+                  </div>
+                </div>
+              </li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Morbi leo risus</li>
+              <li class="list-group-item">Porta ac consectetur ac</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            </div> 
           </div>
     </div>
   );
