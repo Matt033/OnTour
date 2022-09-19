70b7d4285517b8bd4cc4ac3fcc8c3c1b70cbb39b
Kwanwoo Baek <kwanwoob@usc.edu>
2022-09-18 17:12:57 -0700

    Hard coded upcoming and past show schedule

--- ontour/src/pages/Artist.jsx
+++ ontour/src/pages/Artist.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../index.css';
+
 function Artist() {
   return (
     <div className="artist">
@@ -66,12 +67,31 @@ function Artist() {
             </a>
 
             <a href="/review">
-              <div class="row justify-content-center py-3">
-                <u>
-                  See more upcoming shows
-                </u>
+              <div class="row justify-content-center py-3 show">
+                <div class="col-2">
+                  <button type="button" class="date-btn btn btn-secondary">
+                    <h6>
+                      Oct 23
+                    </h6>
+                  </button>
+                </div>
+                <div class="col-8 show-location">
+                  <div class="font-weight-bold">
+                    Super Legends Cruise 2023
+                  </div>
+                  <div>
+                    Long Beach, California
+                  </div>
+                </div>
+                <div class="col-2 align-self-center">
+                  <img src="https://assets.sk-static.com/images/nw/furniture/icons/chevron-black.svg"></img>
+                </div>
               </div>
             </a>
+
+            <div class="row justify-content-center py-3">
+              <button id="upcoming-btn">See more</button>
+            </div>
           </div>
 
           <div class="container py-5 shows">
@@ -90,10 +110,33 @@ function Artist() {
                 </div>
                 <div class="col-8 show-location">
                   <div class="font-weight-bold">
-                    Primavera Sound LA 2021
+                    Primavera Sound LA 2020
                   </div>
                   <div>
                     Los Angeles, California
+                  </div>
+                </div>
+                <div class="col-2 align-self-center">
+                  <img src="https://assets.sk-static.com/images/nw/furniture/icons/chevron-black.svg"></img>
+                </div>
+              </div>
+            </a>
+
+            <a href="/review">
+              <div class="row justify-content-center py-3 show">
+                <div class="col-2">
+                  <button type="button" class="date-btn btn btn-secondary">
+                    <h6>
+                      Sep 15
+                    </h6>
+                  </button>
+                </div>
+                <div class="col-8 show-location">
+                  <div class="font-weight-bold">
+                    Super Legends Cruise 2020
+                  </div>
+                  <div>
+                    Long Beach, California
                   </div>
                 </div>
                 <div class="col-2 align-self-center">
@@ -125,13 +168,9 @@ function Artist() {
               </div>
             </a>
 
-            <a href="/review">
-              <div class="row justify-content-center py-3">
-                <u>
-                  See more past shows
-                </u>
-              </div>
-            </a>
+            <div class="row justify-content-center py-3">
+              <button id="past-btn">See more</button>
+            </div>
           </div>
           
     </div>
